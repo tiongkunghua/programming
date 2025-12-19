dcc5647ba15e54b37afeb8a92e1a6ee39068c3b8
dolores2808 <dolores020828@gmail.com>
2025-12-19 11:17:34 +0800

    Update file3.py

--- file3.py
+++ file3.py
@@ -1,8 +1,6 @@
-def sum_to_n(n):
-    total = 0
-    for i in range(1, n + 1):
-        total += i
-    return total
+from file1 import sum_to_n
+
 
 result_50_100 = sum_to_n(100) - sum_to_n(49)
 print("50 到 100 的總和 =", result_50_100)
+
