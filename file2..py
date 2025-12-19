0b3b3ab9d596b0153bc138b7f38cb7d5da1417c9
YiZhen <160828357+yizhen983@users.noreply.github.com>
2025-12-19 10:58:55 +0800

    Update and rename fule2..py to file2..py

---
+++ file2..py
@@ -0,0 +1,10 @@
+# def sum_to_n(n):
+#     total = 0
+#     for i in range(1, n + 1):
+#         total += i
+#     return total
+
+from file1 import sum_to_n
+result = sum_to_n(100)
+print("1 到 100 的總和 =", result)
+
